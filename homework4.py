0a1106fe783ceec767a49e8d4d9198dc0f25fc97
kyltiapka321 <168179360+kyltiapka321@users.noreply.github.com>
2024-04-28 14:10:35 +0300

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,8 +1,8 @@
 immutable_var = 55555, 7, 'я балбес', 4.0
-print(immutable_var)
+print('Immutable tuple: ' +  str(immutable_var))
 #immutable_var[2]  = 'я не балбес'  - значения элементов кортежа нельзя изменить потому что они не предназначены для этого, тоесть нее поддерживают изменение по элементам
 #print(immutable_var)
 mutable_list   = [243, 6, 'хлеб люблю', 78.3]
-print(mutable_list)
+print('Mutable list: ' + str(mutable_list))
 mutable_list[2] = 'Обажаю хлеб'
-print(mutable_list)
+print('Mutable list: ' + str(mutable_list))
